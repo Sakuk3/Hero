c1cb5c0850ac6b46fd04b533e4ba592a645c09ef
Lukas Fritsch <lukas.fritsch30@gmail.com>
2018-11-13 17:32:11 +0100

    Show tabs added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import keymap
 from tab import Tab
 
 def main(stdscr):
-    tabs = [None] * 10
+    tabs = [None] * 9
     selected_tab = 0
     tabs[0] = Tab(os.path.dirname(os.path.abspath(__file__)))
     curses.curs_set(False)
@@ -16,7 +16,6 @@ def main(stdscr):
     preview_hight       = curses.LINES-1
     border_width        = 1
     window_parent_dir = curses.newwin(  parent_dir_hight,   parent_dir_width,   1,  0)
-    window_tabs = curses.newwin(        parent_dir_hight,   parent_dir_width,   1,  0)
     window_current_dir = curses.newwin( current_dir_hight,  current_dir_width,  1,  parent_dir_width+border_width)
     window_preview = curses.newwin(     preview_hight,      preview_width,      1,  current_dir_width+preview_width+border_width)
     # init colors
@@ -45,9 +44,9 @@ def main(stdscr):
             if(tabs[selected_tab].get_selected_item_index() < len(os.listdir(tabs[selected_tab].get_current_path()))-1):
                 tabs[selected_tab].set_selected_item_index(tabs[selected_tab].get_selected_item_index() + 1)
         elif(key in keymap.K_TABS):
-            if tabs[int(key)] == None:
-                tabs[int(key)] = Tab(os.path.dirname(os.path.abspath(__file__)))
-            selected_tab = int(key)
+            if tabs[int(key)-1] == None:
+                tabs[int(key)-1] = Tab(os.path.dirname(os.path.abspath(__file__)))
+            selected_tab = int(key)-1
 
 
 def draw_screen(stdscr,tabs,selected_tab,window_parent_dir,window_current_dir,window_preview):
@@ -55,12 +54,23 @@ def draw_screen(stdscr,tabs,selected_tab,window_parent_dir,window_current_dir,wi
 
     # Draw current Path on top
     if(tabs[selected_tab].get_current_path() != '/'):
-        pass
         stdscr.addstr(0,0,tabs[selected_tab].get_current_path()+'/'+os.listdir(tabs[selected_tab].get_current_path())[tabs[selected_tab].get_selected_item_index()],curses.A_REVERSE)
     else:
         stdscr.addstr(0,0,tabs[selected_tab].get_current_path()+os.listdir(tabs[selected_tab].get_current_path())[tabs[selected_tab].get_selected_item_index()],curses.A_REVERSE)
 
-    stdscr.chgat(-1, curses.A_REVERSE)
+    # Draw tabs on top
+    i = 0
+    for idx,tab in enumerate(tabs):
+        if tab != None:
+            if idx == selected_tab:
+                stdscr.addstr(0,curses.COLS-len([1 for tab in tabs if tab != None])+i,str(idx+1),curses.A_REVERSE)
+            else:
+                stdscr.addstr(0,curses.COLS-len([1 for tab in tabs if tab != None])+i,str(idx+1))
+            i += 1
+
+
+
+    #stdscr.chgat(-1, curses.A_REVERSE)
 
     # display items from parent directory
     if(tabs[selected_tab].get_current_path() != '/'):
