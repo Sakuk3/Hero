44134c41907cba4fc4e84dd87c2635c9431384de
Lukas Fritsch <lukas.fritsch30@gmail.com>
2018-12-06 20:15:19 +0100

    startet ftp tabs

--- tabs.py
+++ tabs.py
@@ -59,3 +59,48 @@ class tab:
             cur_index = os.listdir(self.path).index(os.path.basename(self.selected_item))
             if cur_index  < len(os.listdir(self.path))-1:
                 self.selected_item = os.path.join(self.path,os.listdir(self.path)[cur_index+1])
+
+class ftp_tab(tab):
+    def __init__(self,path,index):
+        super().__init__()
+        self.path = path
+        self.selected = True
+        self.index = index
+        self.selected_item = os.path.join(path,os.listdir(os.path.dirname(os.path.abspath(__file__)))[0])
+
+    def list_current_dir(self):
+        return os.listdir(self.path)
+
+    def list_parent_dir(self):
+        return os.listdir(os.path.dirname(self.path))
+
+    def list_selected_dir(self):
+        if os.path.isdir(self.selected_item):
+            return os.listdir(self.selected_item)
+        else:
+            return[]
+
+    def dir_back(self):
+        if self.path != '':
+            self.selected_item = self.path
+            self.path = os.path.dirname(self.path)
+
+    def dir_next(self):
+        if self.selected_item and os.path.isdir(self.selected_item):
+            self.path = self.selected_item
+            if os.listdir(self.path):
+                self.selected_item = os.path.join(self.path,os.listdir(self.path)[0])
+            else:
+                self.selected_item = False
+
+    def idx_up(self):
+        if self.selected_item:
+            cur_index = os.listdir(self.path).index(os.path.basename(self.selected_item))
+            if cur_index != 0:
+                self.selected_item = os.path.join(self.path,os.listdir(self.path)[cur_index-1])
+
+    def idx_down(self):
+        if self.selected_item:
+            cur_index = os.listdir(self.path).index(os.path.basename(self.selected_item))
+            if cur_index  < len(os.listdir(self.path))-1:
+                self.selected_item = os.path.join(self.path,os.listdir(self.path)[cur_index+1])
